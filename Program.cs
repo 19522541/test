5cb421412f3701c06fbd932264a663d75b4ef0fc
19522541 <67738980+19522541@users.noreply.github.com>
2020-09-16 22:39:35 +0700

    1st

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using classA;
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            classs a = new classs();
+            a.test();
+
+        }
+    }
+}
---
+++ classA.cs
@@ -0,0 +1,12 @@
+namespace classA
+{
+    public class classs
+    {   
+        public classs (){
+            System.Console.WriteLine("dc1");
+        }
+        internal void test(){
+            System.Console.WriteLine("dc ko");
+        }
+    }
+}
